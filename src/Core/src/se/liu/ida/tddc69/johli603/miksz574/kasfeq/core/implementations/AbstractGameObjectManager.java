ebd6f716a6e917b3c603390eba5d023f7c309be1
miksz574 <miksz574@student.liu.se>
2013-09-19 21:45:29 +0200

    Implemented a simple list based storage for the AbstractGameObjectManager class

--- src/Core/src/se/liu/ida/tddc69/johli603/miksz574/kasfeq/core/implementations/AbstractGameObjectManager.java
+++ src/Core/src/se/liu/ida/tddc69/johli603/miksz574/kasfeq/core/implementations/AbstractGameObjectManager.java
@@ -1,8 +1,18 @@
 package se.liu.ida.tddc69.johli603.miksz574.kasfeq.core.implementations;
 
+import se.liu.ida.tddc69.johli603.miksz574.kasfeq.core.GameObject;
 import se.liu.ida.tddc69.johli603.miksz574.kasfeq.core.interfaces.GameObjectManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class AbstractGameObjectManager implements GameObjectManager {
+    private List<GameObject> gameObjects;
+
+    protected AbstractGameObjectManager() {
+        gameObjects = new ArrayList<GameObject>();
+    }
+
     /**
      * \brief Initializes the component
      */
@@ -22,5 +32,10 @@ public abstract class AbstractGameObjectManager implements GameObjectManager {
      */
     @Override
     public void destroy() {
+        for(GameObject object : gameObjects) {
+            object.dispose();
+        }
+
+        gameObjects.clear();
     }
 }
