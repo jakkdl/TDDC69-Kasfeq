cadecd894a34ca08d2f1aefa6af125ed085ced6d
miksz574 <miksz574@student.liu.se>
2013-09-19 21:15:46 +0200

    Fixed missing empty implementations for the OpenGLRenderingEngine class

--- src/Core/src/se/liu/ida/tddc69/johli603/miksz574/kasfeq/core/implementations/OpenGLRenderingEngine.java
+++ src/Core/src/se/liu/ida/tddc69/johli603/miksz574/kasfeq/core/implementations/OpenGLRenderingEngine.java
@@ -3,4 +3,24 @@ package se.liu.ida.tddc69.johli603.miksz574.kasfeq.core.implementations;
 import se.liu.ida.tddc69.johli603.miksz574.kasfeq.core.interfaces.RenderingEngine;
 
 public class OpenGLRenderingEngine implements RenderingEngine {
+    /**
+     * \brief Initializes the component
+     */
+    @Override
+    public void initialize() {
+    }
+
+    /**
+     * \brief Updates the component
+     */
+    @Override
+    public void update() {
+    }
+
+    /**
+     * \brief Destroys the component
+     */
+    @Override
+    public void destroy() {
+    }
 }
