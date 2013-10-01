330f67eff375ccb61a726ff26e4912ebf158f6cd
miksz574 <miksz574@student.liu.se>
2013-10-01 12:21:35 +0200

    Cleanup and initial work

--- src/Core/src/se/liu/ida/tddc69/johli603/miksz574/kasfeq/core/Game.java
+++ src/Core/src/se/liu/ida/tddc69/johli603/miksz574/kasfeq/core/Game.java
@@ -1,6 +1,9 @@
 package se.liu.ida.tddc69.johli603.miksz574.kasfeq.core;
 
 import org.newdawn.slick.*;
+import se.liu.ida.tddc69.johli603.miksz574.kasfeq.core.interfaces.GameComponent;
+import se.liu.ida.tddc69.johli603.miksz574.kasfeq.core.world.World;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,31 +12,46 @@ import java.util.logging.Logger;
  *
  *  Contains the main entry point and handles initialization of everything
  */
-class Game extends BasicGame {
+class Game implements org.newdawn.slick.Game {
+    private GameComponent activeComponent;
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+        gameContainer.setShowFPS(false);
+        // Enter game initially(Testing purposes)
+        activeComponent = new World();
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+        activeComponent.update(gameContainer, i);
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+        activeComponent.render(gameContainer, graphics);
+    }
+
+    @Override
+    public boolean closeRequested() {
+        // Cleanup
+        // Return true if the game should close
+        return true;
+    }
+
+    @Override
+    public String getTitle() {
+        return "Kasfeq";
+    }
+
     /** \brief Main entry point for the game */
     public static void main(String[] args) {
         try {
-            AppGameContainer container = new AppGameContainer(new Game("Kasfeq"));
+            AppGameContainer container = new AppGameContainer(new Game());
             container.start();
         } catch(SlickException e) {
             Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, e);
         }
     }
 
-    @Override
-    public void init(GameContainer gameContainer) throws SlickException {
-        gameContainer.setShowFPS(false);
-    }
-
-    @Override
-    public void update(GameContainer gameContainer, int i) throws SlickException {
-    }
-
-    @Override
-    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-    }
-
-    private Game(String gameName) {
-        super(gameName);
-    }
 }
---
+++ src/Core/src/se/liu/ida/tddc69/johli603/miksz574/kasfeq/core/interfaces/GameComponent.java
@@ -0,0 +1,8 @@
+package se.liu.ida.tddc69.johli603.miksz574.kasfeq.core.interfaces;
+
+import org.newdawn.slick.*;
+
+public interface GameComponent {
+    public void update(GameContainer gameContainer, int i) throws SlickException;
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException;
+}
